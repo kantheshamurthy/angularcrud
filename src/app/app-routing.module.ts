712b7d56fb8414c98e5963a778c8aab3084882bc
Kanthesha S B. Murthy <kanthesha@cce.soft.com>
2018-12-19 15:53:20 +0530

    after getting business/add error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,23 @@
+// app-routing.module.ts
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { GstAddComponent } from './gst-add/gst-add.component'
+import { GstAddComponent } from './gst-add/gst-add.component';
 import { GstEditComponent } from './gst-edit/gst-edit.component';
 import { GstGetComponent } from './gst-get/gst-get.component';
 
 const routes: Routes = [
   {
-    path:'business/create-add',
-    component:GstAddComponent
+    path: 'business/create',
+    component: GstAddComponent
   },
   {
-    path:'business/edit/:id',
+    path: 'business/edit/:id',
     component: GstEditComponent
   },
   {
-    path:'business',
-    component:GstGetComponent
+    path: 'business',
+    component: GstGetComponent
   }
 ];
 
@@ -23,4 +25,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
